0072471e5e719e3fe09bae51d65602a923e04b1b
Omar Hafsa <omar.hafsa2003@gmail.com>
2024-04-08 10:32:54 +0100

    save changes

--- src/compenent/footer.js
+++ src/compenent/footer.js
@@ -3,7 +3,7 @@ export default function footer(){
         <footer>
           <h1>Hafsa</h1>
           <div className="footericon">
-            <img src="/images/icons/icons8-facebook-50.png"></img>
+            <img src="images/icons/icons8-facebook-50.png"></img>
             <img src="images/icons/icons8-github-50.png"></img>
             <img src="images/icons/icons8-instagram-50.png"></img>
           </div>
